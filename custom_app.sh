59e297dd85d1fb63c4941770964d35776fcb3474
pochan <5851382@qq.com>
2015-02-10 21:26:54 +0800

    allow MassStorage enable when has a Sdcard in Phone

--- custom_app.sh
+++ custom_app.sh
@@ -9,6 +9,9 @@ if [ "$apkBaseName" = "Settings" ];then
 	sed -i -e "/^\.method.*setOfficialVerifyPreference()V/,/^\.end method/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali
 	sed -i -e "/setOfficialVerifyPreference/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali	
 
+    echo ">>> use isHaveExternalSdcard to check weather support UsbMassStorage in UsbSettings.java"
+    sed -i 's#invoke-virtual {v.*}, Landroid/os/storage/StorageVolume;->allowMassStorage()Z#invoke-direct {p0}, Lcom/android/settings/deviceinfo/UsbSettings;->isHaveExternalSdcard()Z#g' $tempSmaliDir/smali/com/android/settings/deviceinfo/UsbSettings.smali
+
     echo ">>> add origin settings"
     sed -i '/com.android.settings.ManageApplicationsSettings/r Settings/settings_headers.xml.part' $tempSmaliDir/res/xml/settings_headers.xml
      
