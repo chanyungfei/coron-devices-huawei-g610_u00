5ff900f85b7bf0860111594f3b0a73e9e2bfa884
pochan <5851382@qq.com>
2015-02-10 16:40:53 +0800

    add SchedulePowerOnOff

--- custom_app.sh
+++ custom_app.sh
@@ -8,6 +8,9 @@ if [ "$apkBaseName" = "Settings" ];then
     echo ">>> Remove the Official Verify Preference."
 	sed -i -e "/^\.method.*setOfficialVerifyPreference()V/,/^\.end method/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali
 	sed -i -e "/setOfficialVerifyPreference/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali	
+
+    echo ">>> add origin settings"
+    sed -i '/com.android.settings.ManageApplicationsSettings/r Settings/settings_headers.xml.part' $tempSmaliDir/res/xml/settings_headers.xml
      
 fi
 
